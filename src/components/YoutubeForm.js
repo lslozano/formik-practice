dab9a00beca7fd957f12fa70c8cfc130c33f9298
Luis Lozano <lslozano11@gmail.com>
2021-06-17 13:43:50 -0500

    Use of FieldArray component and render Field components dynamically

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field, ErrorMessage, FieldArray } from "formik";
 import * as Yup from "yup";
 
 import TextError from './TextError';
@@ -10,6 +10,12 @@ const initialValues = {
   channel: '',
   comments: '',
   address: '',
+  social: {
+    facebook: '',
+    twitter: '',
+  },
+  phoneNumber: ['', ''],
+  phNumbers: [''],
 };
 
 const onSubmit = (values) => {
@@ -51,30 +57,73 @@ const YoutubeForm = () => {
           <label htmlFor='channel'>Channel</label>
           <Field type='text' id='channel' name='channel' />
 
-          <ErrorMessage name='channel' />
+          <ErrorMessage name='channel' component={TextError}/>
         </div>
 
         <div className='form-control'>
           <label htmlFor='comments'>Comments</label>
           <Field as='textarea' id='comments' name='comments' />
 
-          <ErrorMessage name='comments' />
+          <ErrorMessage name='comments' component={TextError}/>
         </div>
 
         <div className='form-control'>
           <label htmlFor='address'>Address</label>
           <Field name='address'>
             {props => {
-                const { field, meta } = props;
+                const { field } = props;
                 return (
                   <div>
                     <input type='text' id='address' {...field}/>
-                    {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+                    <ErrorMessage name='address' component={TextError} />
                   </div>
                 ) 
               }
             }
           </Field>
+        </div>
+
+        <div className='form-control'>
+          <label htmlFor='facebook'>Facebook profile</label>
+          <Field type='text' id='address' name='social.facebook' />    
+        </div>
+
+        <div className='form-control'>
+          <label htmlFor='twitter'>Twitter</label>
+          <Field type='text' id='twitter' name='social.twitter' />
+        </div>
+
+        <div className='form-control'>
+          <label htmlFor='primaryPh'>Primary phone number</label>
+          <Field type='text' id='primaryPh' name='phoneNumber[0]' />    
+        </div>
+
+        <div className='form-control'>
+          <label htmlFor='secondaryPh'>Secondary phone number</label>
+          <Field type='text' id='secondaryPh' name='phoneNumber[1]' />
+        </div>
+
+        <div className='form-control'>
+          <label>List of phone numbers</label>
+          <FieldArray name='phNumbers'>
+            {fieldArrayProps => {
+              const { push, remove, form: { values: { phNumbers }} } = fieldArrayProps;
+              return (
+                <div>
+                  {phNumbers.map((phNumber, index) => {
+                    return (
+                      <div key={index}>
+                        <Field name={`phNumbers[${index}]`} />
+                        {index > 0 && <button type='button'onClick={() => remove(index)}>-</button>}
+                        <button type='button'onClick={() => push('')}>+</button>
+                      </div>
+                    )
+                  })
+                  }
+                </div>
+              )
+            }}
+          </FieldArray>
         </div>
 
         <button type='submit'>Submit</button>
