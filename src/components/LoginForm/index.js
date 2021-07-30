dae545227d6606ac3796e877aa4ddd36aefb9ca6
Luis Lozano <lslozano11@gmail.com>
2021-07-30 16:49:14 -0500

    Create the login form and add some styles

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
+import React from "react";
 
-import ButtonLink from '../ButtonLink';
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+
+import FormikControl from "../FormikControl";
+import ButtonLink from "../ButtonLink";
 
 const LoginForm = () => {
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object({
+    email: Yup.string().required("Required").email(),
+    password: Yup.string().required("Required"),
+  });
+
+  const onSubmit = (values) => {
+    console.log("The values of the form are:", values);
+  };
+
   return (
-    <div>
-      <ButtonLink 
-        path="/"
-        text="Back home"
-      />
-      <h1>Hi there</h1>
+    <div className="login-form">
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        {(formik) => {
+          return (
+            <Form>
+              <FormikControl
+                control="input"
+                type="email"
+                label="Email"
+                name="email"
+              />
+              <FormikControl
+                control="input"
+                type="password"
+                label="Password"
+                name="password"
+              />
+              <button className="link-button" type="submit">
+                Submit
+              </button>
+            </Form>
+          );
+        }}
+      </Formik>
+      <ButtonLink path="/" text="Back home" />
     </div>
-  )
+  );
 };
 
 export default LoginForm;
